7724c9c965d2161be4404ece24e34f72c2526bce
furkanyaman <furkanyaman@d-teknoloji.com.tr>
2024-04-07 18:48:45 +0300

    added tsconfig added components ui added shadcdn ui

---
+++ src/Menu.tsx
@@ -0,0 +1,35 @@
+
+
+import {
+    Menubar,
+    MenubarContent,
+    MenubarItem,
+    MenubarMenu,
+    MenubarSeparator,
+    MenubarShortcut,
+    MenubarTrigger,
+  } from "@/components/ui/menubar"
+export function Menu(){
+
+return (
+ <>
+ <Menubar>
+        <MenubarMenu>
+          <MenubarTrigger>File</MenubarTrigger>
+          <MenubarTrigger>Copy</MenubarTrigger>
+          <MenubarTrigger>Button</MenubarTrigger>
+          <MenubarContent>
+            <MenubarItem>
+              New Tab <MenubarShortcut>⌘T</MenubarShortcut>
+            </MenubarItem>
+            <MenubarItem>Windows</MenubarItem>
+            <MenubarSeparator />
+            <MenubarItem>Share</MenubarItem>
+            <MenubarSeparator />
+            <MenubarItem>Print</MenubarItem>
+          </MenubarContent>
+        </MenubarMenu>
+      </Menubar>
+      </>
+)
+}
