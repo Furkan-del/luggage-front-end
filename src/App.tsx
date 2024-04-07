af3805b48a041f3e512f772b612b611aafa42230
furkanyaman <furkanyaman@d-teknoloji.com.tr>
2024-04-08 00:12:21 +0300

    refactored

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,20 @@ import React, { useState } from 'react'
 import { Slider } from "@/components/ui/slider"
 
 
-import { AuthenticationTab } from './AuthenticationTab'
+import  {TabsDemo}   from './TabsDemo'
 
 import { Luggages } from './Luggages'
 import { SliderDemo } from './Slider'
+import './App.css'
 
-
+import { Theme } from '@radix-ui/themes';
 function App() {
   
   return (
-    <div>
-       <Luggages/>
-       <SliderDemo/>
+    <div style={ {width : '100vw',  display: 'flex' , alignItems: 'center', justifyContent:'center'} }>
+      <Theme>
+      <TabsDemo/>
+      </Theme>
     </div>
   )
 }
