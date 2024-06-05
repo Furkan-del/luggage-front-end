2fb92684b936977c8c2eae262f246fd9033c64d9
furkanyaman <furkanyaman@d-teknoloji.com.tr>
2024-06-06 01:25:22 +0300

    refactor

--- src/components/UserInfoPage.tsx
+++ src/components/UserInfoPage.tsx
@@ -1,19 +1,25 @@
 import React, { useState, useEffect } from 'react';
-import { Box, Heading, Text, Button, Spinner, VStack, Avatar, useColorModeValue, List, ListItem } from '@chakra-ui/react';
+import { Box, Heading, Text, Button, Spinner, VStack, Avatar, useColorModeValue } from '@chakra-ui/react';
 import User from './types/UserType';
 import Address from './types/Address';
 import AppService from './services/AppService';
 
 const UserProfile: React.FC = () => {
-  var initialUser = {
-    fullName:"",
-    mail:"",
-    phoneNumber:"",
-    username:""
+interface UserResponse{
+  id:any,
+    mail: any,
+    phoneNumber: any,
+    fullName: any
 }
 
+  const initialUser = {
+    id:"",
+    mail: "",
+    phoneNumber: "",
+    fullName: "",
+  };
 
-  const [user, setUser] = useState<User>(initialUser);
+  const [user, setUser] = useState<UserResponse>(initialUser);
   const [addresses, setAddresses] = useState<Address[]>([]);
   const [loading, setLoading] = useState(true);
 
@@ -56,9 +62,8 @@ const UserProfile: React.FC = () => {
     <Box p={5} shadow="md" borderWidth="1px" bg={bg} color={color} borderRadius="md">
       <VStack spacing={4}>
         <Avatar size="2xl" name={user.fullName} src="" />
-        <Heading as="h1" size="xl" mb={4}>{user.fullName}'s Profile</Heading>
         <Text fontSize="lg"><strong>Email:</strong> {user.mail}</Text>
-        <Text fontSize="lg"><strong>Username</strong> {user.phoneNumber}</Text>
+        <Text fontSize="lg"><strong>Phone Number:</strong> {user.phoneNumber}</Text>
         {/* Add more user info as needed */}
         <Button mt={4} colorScheme="orange" onClick={() => window.location.href = '/userside/auth/editprofile'}>
           Edit Profile
